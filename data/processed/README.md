ad26e1e59620d2d6279ea13ec5387fa4b02d05d3
unknown <jmald91@gmail.com>
2017-02-26 01:54:01 -0500

    Updated python script to output number of delays per airline and number of delays in time groupings labeled 1 to 9. Each group is a 15 minute interval. Added readme for processed results folder were the python script writes to.

---
+++ data/processed/README.md
@@ -0,0 +1,9 @@
+# Python script results
+
+This folder contains results for the python scripts which will aggregate and transform data.
+
+Since the data files are too large to be uploaded via github, please download the files from the shared google drive folder (link below)
+
+https://drive.google.com/drive/folders/0By4g_AEW_x0ZVmZFWGdZQm9YbDQ?usp=sharing
+
+Make sure you download the files to this github folder in your local machine. The gitignore file excludes all csv files so you don't need to worry about them being uploaded twice.
--- scripts/aggregate_flight_data.py
+++ scripts/aggregate_flight_data.py
@@ -1,28 +1,49 @@
-import csv
+import csv, sys
 from collections import defaultdict
 
 def default_factory():
-    return [0, None, None, 0]
+    return [0, 0, None, None, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 reader = csv.DictReader(open('../data/tests/aggregate_airlines.csv', newline=''))
 airlines = defaultdict(default_factory)
 for row in reader:
-    number_of_delays = 0
     if(row["ARR_DELAY"].isdigit()):
-        delayed_hours = int(row["ARR_DELAY"])
+        delayed_hours = int(row["ARR_DELAY"]) # negative values are parsed as 0
     else:
         delayed_hours = 0
+        
+    if (delayed_hours):
+        airlines[row["UNIQUE_CARRIER"]][1] += 1
     
-    print(delayed_hours)
     airlines[row["UNIQUE_CARRIER"]][0] += delayed_hours
-    max = airlines[row["UNIQUE_CARRIER"]][1]
-    airlines[row["UNIQUE_CARRIER"]][1] = delayed_hours if max is None else delayed_hours if delayed_hours > max else max
-    min = airlines[row["UNIQUE_CARRIER"]][2]
-    airlines[row["UNIQUE_CARRIER"]][2] = delayed_hours if min is None else delayed_hours if delayed_hours < min else min
-    airlines[row["UNIQUE_CARRIER"]][3] += 1
+    min = airlines[row["UNIQUE_CARRIER"]][2] if airlines[row["UNIQUE_CARRIER"]][2] != 0 else sys.maxsize
+    airlines[row["UNIQUE_CARRIER"]][2] = delayed_hours if min is None else delayed_hours if delayed_hours < min and delayed_hours > 0 else min
+    max = airlines[row["UNIQUE_CARRIER"]][3]
+    airlines[row["UNIQUE_CARRIER"]][3] = delayed_hours if max is None else delayed_hours if delayed_hours > max else max
+    
+    if(row["ARR_DELAY_GROUP"].isdigit()):
+        if int(row["ARR_DELAY_GROUP"]) == 1:
+            airlines[row["UNIQUE_CARRIER"]][5] += 1
+        elif int(row["ARR_DELAY_GROUP"]) == 2:
+            airlines[row["UNIQUE_CARRIER"]][6] += 1
+        elif int(row["ARR_DELAY_GROUP"]) == 3:
+            airlines[row["UNIQUE_CARRIER"]][7] += 1
+        elif  int(row["ARR_DELAY_GROUP"]) == 4:
+            airlines[row["UNIQUE_CARRIER"]][8] += 1
+        elif int(row["ARR_DELAY_GROUP"]) == 5:
+            airlines[row["UNIQUE_CARRIER"]][9] += 1
+        elif int(row["ARR_DELAY_GROUP"]) == 6:
+            airlines[row["UNIQUE_CARRIER"]][10] += 1
+        elif  int(row["ARR_DELAY_GROUP"]) == 7:
+            airlines[row["UNIQUE_CARRIER"]][11] += 1
+        elif int(row["ARR_DELAY_GROUP"]) == 8:
+            airlines[row["UNIQUE_CARRIER"]][12] += 1
+        elif int(row["ARR_DELAY_GROUP"]) == 9:
+            airlines[row["UNIQUE_CARRIER"]][13] += 1
+        
 for airline in airlines:
-    airlines[airline][3] = airlines[airline][0]/airlines[airline][3] # calculate mean
+    airlines[airline][4] = airlines[airline][0]/airlines[airline][1] # calculate mean
 
-writer = csv.writer(open('out.csv', 'w', newline = ''))
-writer.writerow(["UNIQUE_CARRIER", "ARR_DELAY", "max", "min", "mean"])
+writer = csv.writer(open('../data/processed/delays_cancellations_by_airline.csv', 'w', newline = ''))
+writer.writerow(["AIRLINE", "DELAY_MINS_TOTAL", "NUM_DELAYS", "MIN_DELAY", "MAX_DELAY", "MEAN", "GRP_1_TOTAL", "GRP_2_TOTAL", "GRP_3_TOTAL", "GRP_4_TOTAL", "GRP_5_TOTAL", "GRP_6_TOTAL", "GRP_7_TOTAL", "GRP_8_TOTAL", "GRP_9_TOTAL"])
 writer.writerows([airline] + airlines[airline] for airline in airlines)
